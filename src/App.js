fe1c8f386047a3588b60eeb9e018c1cc99bc2605
samanthakazla <samanthakazla@gmail.com>
2021-02-18 20:17:44 -0600

    Brian asked me to do this

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import style from 'styled-components';
+import Character from './components/Character';
+
+
+const newCard = style.div`
+display : flex;
+flex-wrap: wrap;
+text-align: center;
+align-items: center;
+margin: 0 auto;
+justify-content: space-around;`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [data, setData]= useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+useEffect (() => {
+axios.get("https://swapi.py4e.com/api/people/")
+.then((res) =>{
+console.log(res)
+setData(res.data.results)
+})
+
+.catch((err) => {
+  console.log(err)
+})
+}, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <newCard>
+        <Character data = {data} />
+      </newCard>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React from 'react';
+import styles from 'styled-components';
+
+const CharacterCards = styles.div`
+      background-color: blue;
+      opacity: 80%;
+      color: lightgrey;
+      width: 600px;
+      margin-left: auto;
+      margin-right: auto;
+      border-radius: 20%;
+      margin-bottom: 4%;
+      padding: 2px;0
+`
+
+const Character = props => {
+let {data} = props;
+
+    return(
+        <div>
+            {data.map(starWars =>{
+                return (
+                    <CharacterCards>
+            <div>
+            <h3>Name: {starWars.name}</h3>
+            <h3>Gender: {starWars.gender}</h3>
+            <h3>Birth Year: {starWars.birth_year}</h3>
+            </div>
+            </CharacterCards>
+                )
+})}
+
+        </div>
+
+    )
+}
+export default Character; 
